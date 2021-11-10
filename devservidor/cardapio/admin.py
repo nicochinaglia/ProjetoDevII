f8f1433b982713c5bd1ddf2a9f6860f29cc2b2b3
Nicolas <chinaglianr@gmail.com>
2021-11-10 20:33:26 -0300

    Create models

--- devservidor/cardapio/admin.py
+++ devservidor/cardapio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "recipe", "created", "updated")
+    prepopulated_fields = {"slug": ("title",)}
+
--- devservidor/cardapio/models.py
+++ devservidor/cardapio/models.py
@@ -1,3 +1,11 @@
 from django.http import HttpResponse
+from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.CharField(max_length=255)
+    recipe = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
